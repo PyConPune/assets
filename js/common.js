030e3c5ca61357d5b02a55c120eb5805b1ee1b3e
Chandransh Srivastava <chandransh.s@gmail.com>
2017-07-18 23:49:07 +0530

    improve header responsiveness, show buy tickets button in header on appropritate scroll

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
+    var mobileMenuBtn = $('.mobile-menu-btn');
     /* Mobile menu sidebar hide/show (Start) */
     var bodyObject = $('body');
-    var mobileMenuBtn = $('.mobile-menu-btn');
     if(mobileMenuBtn.is(':visible')) {
         function toggleMobileSidebar() {
             mobileMenuBtn.toggleClass('close-icon');
@@ -14,4 +14,22 @@ $(document).ready(function () {
         });
     }
     /* Mobile menu sidebar hide/show (End) */
+
+    /* Show Buy Tickets button in header when header overlaps
+    Buy Tickets button in hero-container in desktops and tablets (Start) */
+	var menuBuyTickets = $('.nav-menu .buy-tickets-btn').parent();
+    if(mobileMenuBtn.is(':visible')) {
+		// Always show Buy Tickets button in mobile sidebar
+		menuBuyTickets.removeClass('hide');
+    } else {
+		$(window).on('scroll', function() {
+		    if($(this).scrollTop() >= $('.hero-container .buy-tickets-btn').offset().top){
+		        menuBuyTickets.removeClass('hide');
+		    } else {
+				menuBuyTickets.addClass('hide');
+		    }
+		});
+    }
+    /* Show Buy Tickets button in header when header overlaps
+    Buy Tickets button in hero-container in desktops and tablets (End) */
 });
