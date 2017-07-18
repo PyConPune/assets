189bd1e6c3c5a2a1ba641598dccebecd76340b10
Chandransh Srivastava <chandransh.s@gmail.com>
2017-07-18 11:47:50 +0530

    add responsive header

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-    var documentObject = $(document);
+    /* Mobile menu sidebar hide/show (Start) */
+    var bodyObject = $('body');
+    var mobileMenuBtn = $('.mobile-menu-btn');
+    if(mobileMenuBtn.is(':visible')) {
+        function toggleMobileSidebar() {
+            mobileMenuBtn.toggleClass('close-icon');
+            bodyObject.toggleClass('slide-right').toggleClass('no-scroll');
+        }
+        mobileMenuBtn.on('click', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            toggleMobileSidebar();
+        });
+    }
+    /* Mobile menu sidebar hide/show (End) */
 });
